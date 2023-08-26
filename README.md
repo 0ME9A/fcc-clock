a02af19ac8ee73b5517c207ac805fa0e121ab8bf
Baliram <heyome9a@gmail.com>
2023-08-26 23:23:11 +0530

    fcc clock done

--- README.md
+++ README.md
@@ -1,34 +1,52 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# FCC Clock
 
-## Getting Started
+![FCC Clock Preview](FCCClock.png)
 
-First, run the development server:
+FCC Clock is a timer application that helps you manage your time effectively using customizable session and break lengths. Stay productive and organized with this interactive countdown timer.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+## Table of Contents
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- [Demo](#demo)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Demo
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Try out the FCC Clock here: [Live Demo](https://clock.ome9a.com)
 
-## Learn More
+## Features
 
-To learn more about Next.js, take a look at the following resources:
+- Customize session and break lengths according to your preference.
+- Interactive user interface for easy time management.
+- Utilize the Pomodoro technique to enhance productivity.
+- Countdown timer displays remaining time in mm:ss format.
+- Audio alert when the timer reaches zero.
+- Pause and resume functionality for better control.
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## Installation
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+1. Clone the repository: `git clone https://github.com/0me9a/fcc-clock`
+2. Navigate to the project directory: `cd fcc-clock`
+3. Install dependencies: `npm install`
 
-## Deploy on Vercel
+## Usage
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+1. Start the development server: `npm run dev`
+2. Open your browser and visit: `http://localhost:3000`
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Customize session and break lengths using the increment and decrement buttons. Click the "Start/Pause" button to control the timer. Click the "Reset" button to reset the timer and lengths.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues and pull requests to suggest enhancements, report bugs, or propose new features.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Created with ❤️ by [Baliram Singh](https://ome9a.com)
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,199 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { BsFillPauseFill, BsFillPlayFill, BsPlus } from "react-icons/bs";
+import { LuTimerReset } from "react-icons/lu";
+import { useState, useEffect } from "react";
+import { FiMinus } from "react-icons/fi";
+
+const Home: React.FC = () => {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+  const [timeLeft, setTimeLeft] = useState(sessionLength * 60);
+  const [timerLabel, setTimerLabel] = useState("Session");
+  const [isActive, setIsActive] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
+
+  console.log(isActive);
+
+  useEffect(() => {
+    let countdown: NodeJS.Timeout | undefined;
+
+    if (isActive && timeLeft > 0) {
+      countdown = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    } else if (isActive && timeLeft === 0) {
+      const audio = document.getElementById("beep") as HTMLAudioElement;
+      audio.play();
+
+      setIsBreak(!isBreak);
+      setTimerLabel(isBreak ? "Session" : "Break");
+      setTimeLeft(isBreak ? sessionLength * 60 : breakLength * 60);
+    } else {
+      clearInterval(countdown);
+    }
+
+    return () => clearInterval(countdown);
+  }, [isActive, timeLeft, isBreak, sessionLength, breakLength]);
+
+  const handleReset = () => {
+    const audio = document.getElementById("beep") as HTMLAudioElement;
+    audio.pause();
+    audio.currentTime = 0;
+
+    setIsActive(false);
+    setIsBreak(false);
+    setBreakLength(5);
+    setSessionLength(25);
+    setTimeLeft(25 * 60);
+    setTimerLabel("Session");
+  };
+
+  useEffect(() => {
+    setTimeLeft(sessionLength * 60);
+  }, [sessionLength]);
+
+  const handleSessionLength = (session: "I" | "D") => {
+    if (!isActive) {
+      if (session === "D") {
+        setSessionLength(Math.max(1, sessionLength - 1));
+      }
+      if (session === "I") {
+        setSessionLength(Math.min(60, sessionLength + 1));
+      }
+    }
+  };
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const remainingSeconds = (seconds % 60).toString().padStart(2, "0");
+    return `${minutes}:${remainingSeconds}`;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex items-center justify-center min-h-screen h-full p-3 relative z-10 ">
+      <section className="pt-8 p-4 bg-yellow-950/0 shadow-xl shadow-yellow-800/50 rounded-3xl max-w-[600px] w-full border-2 border-yellow-900">
+        {/* FCC Clock display */}
+        <section className="text-center relative max-w-[350px] w-full mx-auto">
+          <button
+            aria-label="FCC clock display and button"
+            onClick={() => setIsActive(!isActive)}
+            title="FCC Clock with play/pause button"
+            id="start_stop"
+            className="w-full border-4 border-yellow-500 relative bg-yellow-600 shadow-xl shadow-yellow-500/50 aspect-square rounded-full flex flex-col items-center justify-center"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <div id="time-left" className="text-[4rem] font-bold mt-4">
+              {formatTime(timeLeft)}
+            </div>
+            <h3 id="timer-label" className="text-xl font-semibold">
+              {timerLabel}
+            </h3>
+            <div className="text-4xl scale-100 hover:scale-125 absolute bottom-5 aspect-square rounded-full p-2">
+              {isActive ? <BsFillPauseFill /> : <BsFillPlayFill />}
+            </div>
+          </button>
+          <button
+            id="reset"
+            onClick={handleReset}
+            title="reset"
+            className="bg-red-600 border-2 hover:border-red-600 border-transparent hover:bg-red-600/70 text-white p-3 text-2xl rounded-full absolute right-0 bottom-0"
+          >
+            <LuTimerReset />
+          </button>
+        </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        {/* FCC Clock setting */}
+        <section
+          className={`flex justify-center items-center space-x-3 mt-12 rounded-xl shadow-lg p-5 ${
+            isActive ? "opacity-20 cursor-not-allowed" : "opacity-100"
+          }`}
+        >
+          {/* break length setting */}
+          <div className="text-center w-full flex flex-col justify-center items-center gap-3">
+            <div className="w-32 aspect-square rounded-full flex overflow-hidden relative">
+              <button
+                id="break-decrement"
+                title="decrease break length"
+                disabled={isActive ? true : false}
+                onClick={() =>
+                  !isActive && setBreakLength(Math.max(1, breakLength - 1))
+                }
+                className="w-full border-4 text-yellow-700 border-yellow-800 hover:border-yellow-900 rounded-l-full flex items-center justify-start text-2xl pl-2"
+              >
+                <FiMinus />
+              </button>
+              <span className="absolute text-xl left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] bg-yellow-700 p-2 rounded-full w-14 aspect-square flex items-center justify-center">
+                {breakLength}
+              </span>
+              <button
+                id="break-increment"
+                title="increase break length"
+                disabled={isActive ? true : false}
+                onClick={() =>
+                  !isActive && setBreakLength(Math.min(60, breakLength + 1))
+                }
+                className="w-full bg-yellow-800 hover:bg-yellow-900 rounded-r-full border-2 border-transparent flex items-center justify-end text-2xl pr-2"
+              >
+                <BsPlus />
+              </button>
+            </div>
+            <h2
+              id="session-label"
+              className=" text-sm sm:text-lg font-semibold"
+            >
+              Break Length
+            </h2>
+          </div>
+
+          <span className="block h-20 w-1 bg-yellow-900/50" id="divider"></span>
+          {/* session length setting */}
+          <div className="text-center w-full flex flex-col justify-center items-center gap-3 ">
+            <div
+              id="session-length"
+              className="text-xl p-1 aspect-square border-2 border-yellow-600 w-20 rounded-full flex items-center justify-center bg-yellow-800 shadow-xl shadow-yellow-600/50"
+            >
+              {sessionLength}
+            </div>
+            <div className="relative rounded-full overflow-hidden flex items-center justify-center w-full">
+              <button
+                id="session-decrement"
+                title="decrease session length"
+                disabled={isActive ? true : false}
+                className="p-4 rounded-l-full text-xl w-full bg-yellow-900 flex items-center justify-center border-2 border-r-0 border-transparent hover:border-yellow-600 group"
+                onClick={() => handleSessionLength("D")}
+              >
+                <span className="group-hover:scale-125">
+                  <FiMinus />
+                </span>
+              </button>
+              <button
+                id="session-increment"
+                title="increase session length"
+                className="p-4 rounded-r-full text-xl w-full flex items-center justify-center bg-yellow-900 border-2 border-l-0 border-transparent hover:border-yellow-600 group"
+                disabled={isActive ? true : false}
+                onClick={() => handleSessionLength("I")}
+              >
+                <span className="group-hover:scale-125">
+                  <BsPlus />
+                </span>
+              </button>
+            </div>
+            <h2 id="session-label" className="text-sm sm:text-lg font-semibold">
+              Session Length
+            </h2>
+          </div>
+        </section>
+
+        {/* beep sound when clock finish a session length*/}
+        <audio
+          id="beep"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
